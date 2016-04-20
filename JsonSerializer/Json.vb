9fff3e7ba4afd25adf392f063d93a658bd910997
mimusyst <mimu@mimsys.net>
2016-04-20 22:51:59 +0900

    Created

---
+++ JsonSerializer/Json.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Runtime.Serialization.Json
+Imports System.IO
+Imports System.Text
+
+Public Class Json
+    Public Function JsonSerial(ByRef JsonStr As String, ByRef YourType As Type) As Object
+        Dim Obj As Object = Nothing
+        Try
+            Dim ser As DataContractJsonSerializer
+            ser = New DataContractJsonSerializer(YourType)
+            Dim stream1 As New MemoryStream(Encoding.UTF8.GetBytes(JsonStr))
+            Obj = ser.ReadObject(stream1)
+        Catch ex As Exception
+            Return Nothing
+        End Try
+        Return Obj
+    End Function
+End Class
---
+++ test/Module1.vb
@@ -0,0 +1,22 @@
+﻿Module Module1
+
+    <System.Runtime.Serialization.DataContractAttribute()> _
+    Public Class testClass
+        <System.Runtime.Serialization.DataMember()> _
+        Public name As String
+        <System.Runtime.Serialization.DataMember()> _
+        Public gender As String
+        <System.Runtime.Serialization.DataMember()> _
+        Public age As Integer
+
+    End Class
+
+    Sub Main()
+        Dim json As New JsonSerializer.Json
+        Dim test As testClass
+        Dim JsonStr As String = "{ ""name"":""apple"", ""gender"":""man"", ""age"":150}"
+
+        test = json.JsonSerial(JsonStr, GetType(testClass))
+        Debug.Print(String.Format("name={0}, gender={1}, age={2}", test.name, test.gender, test.age))
+    End Sub
+End Module
